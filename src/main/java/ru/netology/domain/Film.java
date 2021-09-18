051a6bf6e5ad0b37ebab53a37e3bfe1849a48f72
roandr1970 <ambal007a@yandex.ru>
2021-09-18 21:08:53 +0300

    Added classes and methods

---
+++ src/main/java/ru/netology/domain/Film.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+
+    public class Film {
+        private int id;
+        private int productId;
+        private String productName;
+        private String genre;
+
+        public Film(int id, int productId, String productName, String genre) {
+            this.id = id;
+            this.productId = productId;
+            this.productName = productName;
+            this.genre = genre;
+        }
+    }
+
---
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -0,0 +1,40 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Film;
+
+public class FilmManager {
+
+    private int numberOutputMovies = 10;
+    private Film[] items = new Film[0];
+
+    public FilmManager(int numberOutputMovies) {
+        this.numberOutputMovies = numberOutputMovies;
+    }
+
+    public FilmManager() {
+    }
+
+    public void add(Film item) {
+            int length = items.length + 1;
+            Film[] tmp = new Film[length];
+            System.arraycopy(items, 0, tmp, 0, items.length);
+            int lastIndex = tmp.length - 1;
+            tmp[lastIndex] = item;
+            items = tmp;
+        }
+
+        public Film[] getAll() {
+            int resultLength;
+            if (items.length < numberOutputMovies) {
+                resultLength = items.length;
+            } else resultLength = numberOutputMovies;
+            Film[] result = new Film[resultLength];
+            for (int i = 0; i < result.length; i++) {
+                int index = items.length - i - 1;
+                result[i] = items[index];
+            }
+            return result;
+        }
+
+    }
+
